85e2cf287ea6822eeec8a725c03156b7c77e089f
zz06282 <sswmirage1@gmail.com>
2019-05-11 22:56:16 +0900

    React Project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Componet {
+    rednder() {
+        return (
+            <div>
+                <h3>Hello World</h3>
+            </div>
+        );
+    }
+}
+
+export default App;
+
